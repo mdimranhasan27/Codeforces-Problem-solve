74d3c4ffd870f0f7819c51d1d1c654b444d9a7ff
imran <mdimranhasan2023@gmail.com>
2025-12-11 01:11:13 +0600

    11Dec2025

---
+++ 1537B.cpp
@@ -0,0 +1,22 @@
+//~imran~
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout << 1 << " " << 1 << " " << n << " " << m << "\n";
+    }
+    return 0;
+}
