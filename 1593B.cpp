1fb86ba575db742f1ae2cb068a9b0502f1974840
imran <mdimranhasan2023@gmail.com>
2025-12-13 14:34:43 +0600

    13Dec2025

---
+++ 1593B.cpp
@@ -0,0 +1,25 @@
+//~imran~
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            if(str[i]=='5')
+        }
+    }
+    return 0;
+}
