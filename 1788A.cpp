e53bc85134160c41031276040f6919c7aae4a922
imran <mdimranhasan2023@gmail.com>
2025-12-14 15:42:29 +0600

    14Dec2025

--- 1788A.cpp
+++ 1788A.cpp
@@ -22,35 +22,29 @@ int main()
         {
             cin >> v[i];
         }
-        vector<int> p(n);
-        vector<int> f(n);
-        f[0] = v[0];
-        for (int i = 1; i < n; i++)
-        {
-            f[i] = f[i - 1] * v[i];
-        }
-        p[n - 1] = v[n - 1];
-        for (int i = n - 2; i >= 0; i--)
-        {
-            p[i] = p[i + 1] * v[i];
-        }
+        ll cnt = 0;
 
-        /*for(int x:f)cout<<x<<" ";
-        cout<<"\n";
-        for(int y:p)cout<<y<<" ";
-        cout<<"\n";*/
-        bool ok = true;
-        for (int i = 0; i < n - 1; i++)
+        for (int i = 0; i < n; i++)
         {
-            if (p[i + 1] == f[i])
+            if (v[i] == 2)
             {
-                cout << i + 1 << "\n";
-                ok = false;
+                cnt++;
+            }
+        }
+        ll ct = 0, ans = -1;
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] == 2)
+            {
+                ct++;
+            }
+            if (ct == cnt - ct)
+            {
+                ans = i + 1;
                 break;
             }
         }
-        if (ok)
-            cout << -1 << "\n";
+        cout << ans << "\n";
     }
     return 0;
 }
