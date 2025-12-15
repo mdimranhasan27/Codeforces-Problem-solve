6ca4c3ae61f5c750c8744e494fbd4ba21d26cd6d
imran <mdimranhasan2023@gmail.com>
2025-12-15 16:17:10 +0600

    15Dec2025

---
+++ 1679A.cpp
@@ -0,0 +1,24 @@
+//~imran~
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+    ll n;
+    cin>>n;
+    if(n>=4 && n%2==0){
+     cout<<(n+6-1)/6<<" "<<n/4<<"\n";
+    }
+    else cout<<"-1\n";
+    }
+    return 0;
+}
